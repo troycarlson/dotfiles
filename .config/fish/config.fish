ed90548329bbdb9268b127f403abb2bbc6d50bfd
Troy Carlson <troycarlson4@gmail.com>
2019-03-06 08:31:25 -0800

    Make rbenv play nice with fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
-set default_user "troycarlson"
+set default_user "tcarlson"
 
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/functions.fish
+
+# rbenv configuration
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1

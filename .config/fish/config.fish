3a0b804eb00bf6f8c16feb876109b3f5983386f4
Troy Carlson <troycarlson4@gmail.com>
2020-07-07 21:56:56 -0700

    Conditional Postgres settings in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 set default_user "troycarlson"
 fish_vi_key_bindings
 
-source ~/.config/fish/aliases.fish
 source ~/.config/fish/functions.fish
+source ~/.config/fish/aliases.fish
 source ~/.config/fish/keyboard.fish
 
 # Ruby config
@@ -13,10 +13,12 @@ else
     echo "[config.fish] Couldn't find Ruby, skipping Ruby config"
 end
 
-# Postgres
-set PATH /usr/local/opt/postgresql@11/bin $PATH
+# Postgres config
+if test (which psql)
+    set PATH /usr/local/opt/postgresql@11/bin $PATH
+    set -g fish_user_paths "/usr/local/opt/libpq/bin" $fish_user_paths
+end
 
-set -g fish_user_paths "/usr/local/opt/libpq/bin" $fish_user_paths
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/troycarlson/google-cloud-sdk/path.fish.inc' ]; . '/Users/troycarlson/google-cloud-sdk/path.fish.inc'; end
